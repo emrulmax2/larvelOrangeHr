cc5288918ef557900ff188befb6671023117b17d
Emrul Easir <emrulmax2@gmail.com>
2023-03-03 20:04:02 +0600

    add task seeder for db:seed command

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([UserSeeder::class,
-        TodoSeeder::class
+        TodoSeeder::class,
+        TaskSeeder::class
         ]);
     }
 }
